b30bde75c2b42a6d56b3b70925392f12431f110c
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2021-11-08 23:02:11 -0300

    Agrego scripts y correcciones en los playbooks

---
+++ Scripts/pull_repo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This script will do a pull from the repo sistemas-operativos, he's main use is for pull when the system restarts
+
+cd /root/chat-institucional
+
+git pull https://jpilsoftware-uy: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@github.com/jpilsoftware-uy/chat-institucional
+
+
